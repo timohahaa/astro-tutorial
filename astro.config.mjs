c7faf8d5b1eebcf7edbaa578dbcb340202fce01c
Timofey <timabd77@gmail.com>
2026-01-20 21:24:49 +0200

    unit 5

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+      site: "https://fascinating-marigold-9b35d7.netlify.app/"
+});
