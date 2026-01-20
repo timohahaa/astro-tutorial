30b9f7d306d20b033630804c5e98b4dbf82166c1
Timofey <timabd77@gmail.com>
2026-01-20 21:47:31 +0200

    unit 6 done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-      site: "https://fascinating-marigold-9b35d7.netlify.app/"
+  site: "https://fascinating-marigold-9b35d7.netlify.app/",
+  integrations: [preact()]
 });
